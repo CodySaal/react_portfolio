48ed4eb51288616fd25da8c78c527019b1fcde14
Cody <110630101+CodySaal@users.noreply.github.com>
2023-02-01 18:09:29 -0600

    Change basename

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 function App() {
   return (
-    <BrowserRouter basename="/">
+    <BrowserRouter basename="/react_portfolio">
       <Header />
         <Routes>
           <Route path="/" element={<About />} />

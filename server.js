0cfac3faa8b894c1d7b95b7eef8cdbf3a4660f45
Cody <110630101+CodySaal@users.noreply.github.com>
2023-02-26 16:50:04 -0600

    heroku static

--- server.js
+++ server.js
@@ -10,10 +10,11 @@ const app = express();
 const PORT = process.env.PORT || 3001
 app.use(cors());
 app.use(express.json());
-app.use("/", router);
 
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, './build')));
+} else {
+  app.use("/", router);
 }
 
 app.listen(PORT, () => console.log("Server Running"));

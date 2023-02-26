7b0463a1616a17aa92309b2a84893811f1168f48
Cody <110630101+CodySaal@users.noreply.github.com>
2023-02-26 15:45:40 -0600

    Added a server to handle contact form

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require("express");
+const router = express.Router();
+const cors = require("cors");
+const nodemailer = require("nodemailer");
+require("dotenv").config()
+
+
+// server used to send send emails
+const app = express();
+const PORT = process.env.PORT || 3001
+app.use(cors());
+app.use(express.json());
+app.use("/", router);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, './build')));
+}
+
+app.listen(PORT, () => console.log("Server Running"));
+
+
+
+const contactEmail = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    type: "OAuth2",
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+    clientId: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    refreshToken: process.env.REFRESH_TOKEN
+  },
+});
+
+contactEmail.verify((error) => {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log("Ready to Send");
+  }
+});
+
+router.post("/contact", (req, res) => {
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+  const mail = {
+    from: name,
+    to: process.env.EMAIL_USER,
+    subject: "Contact Form Submission - Portfolio",
+    html: `<p>Name: ${name}</p>
+           <p>Email: ${email}</p>
+           <p>Message: ${message}</p>`,
+  };
+  contactEmail.sendMail(mail, (error) => {
+    if (error) {
+      res.json(error);
+    } else {
+      res.json({ code: 200, status: "Message Sent" });
+    }
+  });
+});
--- src/components/Contact.js
+++ src/components/Contact.js
@@ -12,36 +12,63 @@ function Contact() {
 
     const [formDetails, setFormDetails] = useState(formInitialDetails);
 
-    const { name, email, message } = formDetails
+    const [buttonText, setButtonText] = useState("Send");
+
+    const [status, setStatus] = useState({});
+
 
     const [errorMessage, setErrorMessage] = useState("")
     
 
-    const onFormUpdate = (e) => {
-        if (e.target.name === 'email') {
-            const isValid = validateEmail(e.target.value);
-            if (!isValid) {
-                setErrorMessage('Your email is invalid.');
-            } else {
-                setErrorMessage('');
-            }
-        } else {
-            if (!e.target.value.length) {
-                setErrorMessage(`${e.target.name} is required.`);
-            } else {
-                setErrorMessage('');
-            }
-        }
-        if (!errorMessage) {
-            setFormDetails({ ...formDetails, [e.target.name]: e.target.value });
-            console.log('Handle Form', formDetails);
-        }
-    }
+    // const onFormUpdate = (e) => {
+    //     if (e.target.name === 'email') {
+    //         const isValid = validateEmail(e.target.value);
+    //         if (!isValid) {
+    //             setErrorMessage('Your email is invalid.');
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     } else {
+    //         if (!e.target.value.length) {
+    //             setErrorMessage(`${e.target.name} is required.`);
+    //         } else {
+    //             setErrorMessage('');
+    //         }
+    //     }
+    //     if (!errorMessage) {
+    //         setFormDetails({ ...formDetails, [e.target.name]: e.target.value });
+    //         console.log('Handle Form', formDetails);
+    //     }
+    // }
 
+    const onFormUpdate = (category, value) => {
+        setFormDetails({
+            ...formDetails,
+            [category]: value
+        })
+    }
 
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setButtonText("Sending...");
+        let response = await fetch("http://localhost:3001/contact", {
+            method: "POST",
+            headers: {
+                "Content-type": "Application/json;charset=utf-8",
+            },
+            body: JSON.stringify(formDetails),
+        });
+        setButtonText("Send");
+        let result = await response.json();
+        setFormDetails(formInitialDetails);
+        if (result.code === 200) {
+            console.log(result)
+            setStatus({ success: true, message: "Message sent successfully"});
+        } else {
+            console.log(result.PromiseResult.code)
+            setStatus({ success: false, message: "Something went wrong, please try again later."})
+        }
         if (!errorMessage) {
             console.log("Submit Form", formDetails)
         }
@@ -67,32 +94,38 @@ function Contact() {
                                             type="text"
                                             placeholder="Name"
                                             name="name"
-                                            defaultValue={name}
-                                            onBlur={onFormUpdate} />
+                                            value={formDetails.name}
+                                            onChange={(e) => onFormUpdate("name", e.target.value)} />
                                     </Col>
                                     <Col size={12} sm={6} className="px-1">
                                         <input
                                             type="email"
                                             placeholder="Email Address"
                                             name="email"
-                                            defaultValue={email}
-                                            onBlur={onFormUpdate} />
+                                            value={formDetails.email}
+                                            onChange={(e) => onFormUpdate("email", e.target.value)} />
                                     </Col>
                                     <Col size={12} className="px-1">
                                         <textarea
                                             rows="6"
                                             placeholder="Message"
                                             name="message"
-                                            defaultValue={message}
-                                            onBlur={onFormUpdate}>
+                                            value={formDetails.message}
+                                            onChange={(e) => onFormUpdate("message", e.target.value)}>
                                         </textarea>
                                         {errorMessage && (
                                             <div>
                                                 <p className="error-text">{errorMessage}</p>
                                             </div>
                                         )}
-                                        <button type="submit"><span>Send</span></button>
+                                        <button type="submit"><span>{buttonText}</span></button>
                                     </Col>
+                                    {
+                                        status.message &&
+                                        <Col>
+                                            <p className={status.succes === false ? "danger" : "success"}>{status.message}</p>
+                                        </Col>
+                                    }
                                 </Row>
                             </form>
                         </div>
